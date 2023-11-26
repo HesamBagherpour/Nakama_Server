e3e110e681be646173997a64fc0af6329ea902a1
hesam <hesam.hb.bagherpour@gmail.com>
2023-11-26 15:10:50 +0330

    [add]  Authenticate with Device

--- Assets/Scripts/NakamaConnection.cs
+++ Assets/Scripts/NakamaConnection.cs
@@ -1,24 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Nakama;
 using UnityEngine;
 
-public class NakamaConnection : MonoBehaviour
+[CreateAssetMenu(fileName = "NakamaConection", menuName = "server/Nakama")]
+public class NakamaConnection : ScriptableObject   // from mono to scripb;e 
 {
     
-    private string scheme = "http";
-    private string host = "localhost";
-    private int port = 7350;
-    private string serverKey = "defaultkey";
-    private IClient _client;
-    private ISession _session;
+    public string scheme = "http";
+    public string host = "localhost";
+    public int port = 7350;
+    public string serverKey = "defaultkey";
     
-    async void Start()
+    public IClient _client;
+    public ISession _session;
+
+
+    private const string SessionPrefabName = "Nakama.session";
+
+    private const string DeviceUniqueIdentifierPreName = "Nakakama.deviceUniqueIdentifier";
+    // async void Start()
+    // {
+    //     _cient = new Client(scheme, host, plort, serverKey, UnityWebRequestAdapter.Instance);
+    //     _session = await _client.AuthenticateDeviceAsync(SystemInfo.deviceUniqueIdentifier);
+    //     Debug.Log("_client" + _client);
+    //     Debug.Log("_session :" + _session );
+    // }
+
+
+    public async Task Connect()
     {
         _client = new Client(scheme, host, port, serverKey, UnityWebRequestAdapter.Instance);
-        _session = await _client.AuthenticateDeviceAsync(SystemInfo.deviceUniqueIdentifier);
-        Debug.Log("_client" + _client);
-        Debug.Log("_session :" + _session );
+    
+
     }
   
 }
+ 
